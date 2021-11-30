05ec5a9e318168fc58740bf8c68b63454db84eba
codenameakshay <akshaymaurya3006@gmail.com>
2021-11-30 13:57:03 +0530

    add buttons and styles

--- app/src/main/java/com/codenameakshay/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/codenameakshay/kotlincalculator/MainActivity.kt
@@ -8,4 +8,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberAction(view: android.view.View) {}
+    fun operationAction(view: android.view.View) {}
+    fun allClearAction(view: android.view.View) {}
+    fun backSpaceAction(view: android.view.View) {}
+    fun equalsAction(view: android.view.View) {}
 }
